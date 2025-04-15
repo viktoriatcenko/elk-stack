bd8351994daae931406dfbec6b2ece783af0e47b
viktoriatsenko <nashtrenerishka@gmail.com>
2025-04-15 10:13:21 +0300

    init

--- src/main/java/com/javatechie/elk/ElkStackExampleApplication.java
+++ src/main/java/com/javatechie/elk/ElkStackExampleApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,34 +17,12 @@ import java.util.stream.Stream;
 @RestController
 public class ElkStackExampleApplication {
 
-	Logger logger=LoggerFactory.getLogger(ElkStackExampleApplication.class);
+    Logger logger = LoggerFactory.getLogger(ElkStackExampleApplication.class);
 
-    @GetMapping("/getUser/{id}")
-    public User getUserById(@PathVariable int id) {
-		List<User> users=getUsers();
-		User user=users.stream().
-				filter(u->u.getId()==id).findAny().orElse(null);
-		if(user!=null){
-			logger.info("user found : {}",user);
-			return user;
-		}else{
-			try {
-				throw new Exception();
-			} catch (Exception e) {
-				e.printStackTrace();
-				logger.error("User Not Found with ID : {}",id);
-			}
-			return new User();
-		}
-    }
-
-
-    private List<User> getUsers() {
-        return Stream.of(new User(1, "John"),
-				new User(2, "Shyam"),
-				new User(3, "Rony"),
-				new User(4, "mak"))
-				.collect(Collectors.toList());
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        logger.info("Garik, this is new info logs 17:32 ");
+        return ResponseEntity.ok("OK");
     }
 
     public static void main(String[] args) {
